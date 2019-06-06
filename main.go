09c177cd2a99cd24aa54d85b7dcaba9ec90ebb40
John Pettit <john.pettit@cox.net>
2019-06-05 18:30:45 -0700

    Update main.go
    No more random device ids      spacing data out more

--- main.go
+++ main.go
@@ -49,17 +49,23 @@ func main() {
 		return
 	}
 
-	gocqlUUID, _ = gocql.RandomUUID()
+	//gocqlUUID, _ = gocql.RandomUUID()
+	gocqlUUID, _ = gocql.ParseUUID("6ad6dd93-80cb-4614-92b8-34bf5ba65a69")
 	data1.ID = gocqlUUID
-	gocqlUUID, _ = gocql.RandomUUID()
+	//gocqlUUID, _ = gocql.RandomUUID()
+	gocqlUUID, _ = gocql.ParseUUID("297c83c1-3cc6-4034-bcdb-746c88b1aba7")
 	data2.ID = gocqlUUID
-	gocqlUUID, _ = gocql.RandomUUID()
+	//gocqlUUID, _ = gocql.RandomUUID()
+	gocqlUUID, _ = gocql.ParseUUID("7745b8d0-b405-47f4-82d9-770bf08d8836")
 	data3.ID = gocqlUUID
-	gocqlUUID, _ = gocql.RandomUUID()
+	//gocqlUUID, _ = gocql.RandomUUID()
+	gocqlUUID, _ = gocql.ParseUUID("8e5f81e9-400b-41c4-b20a-f4df92291ba2")
 	data4.ID = gocqlUUID
-	gocqlUUID, _ = gocql.RandomUUID()
+	//gocqlUUID, _ = gocql.RandomUUID()
+	gocqlUUID, _ = gocql.ParseUUID("ceb8cbf8-5d12-4214-9086-c47fca19e908")
 	data5.ID = gocqlUUID
-	gocqlUUID, _ = gocql.RandomUUID()
+	//gocqlUUID, _ = gocql.RandomUUID()
+	gocqlUUID, _ = gocql.ParseUUID("f1a8681a-0b69-42e9-a362-317a3abe8467")
 	data6.ID = gocqlUUID
 
 	for i := 0; i < 10000000; {
@@ -86,38 +92,6 @@ func main() {
 		}
 
 		i = i + 1 //add 60 seconds
-		time.Sleep(5 * time.Millisecond)
+		time.Sleep(60 * time.Second)
 	}
-	/*
-		gocqlUUID, _ = gocql.RandomUUID()
-		data.ID = gocqlUUID
-		data.TS = r1.Int31()
-		data.Data = r1.Float32()
-
-		// write data to Cassandra
-		err = session.Query(`INSERT INTO firsttest.tsdata (id, ts, data) VALUES (?, ?, ?)`, data.ID, data.TS, data.Data).Exec()
-
-		if err != nil {
-			fmt.Println("errors", err)
-		} else {
-			fmt.Println("Cassandra Insert 2 Done")
-		}
-
-		gocqlUUID, _ = gocql.RandomUUID()
-		data.ID = gocqlUUID
-		data.TS = r1.Int31()
-		data.Data = r1.Float32()
-
-		// write data to Cassandra
-		err = session.Query(`INSERT INTO firsttest.tsdata (id, ts, data) VALUES (?, ?, ?)`, data.ID, data.TS, data.Data).Exec()
-
-		if err != nil {
-			fmt.Println("errors", err)
-		} else {
-			fmt.Println("Cassandra Insert 3 Done")
-		}
-
-		/*
-			CREATE TABLE tsdata (id UUID, ts int, data float,PRIMARY KEY (id, ts));
-	*/
 }
